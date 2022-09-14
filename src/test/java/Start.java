1463bd80943bba290dcf0ce71d4e3ec470dab00c
Yuliia512 <112338873+Yuliia512@users.noreply.github.com>
2022-09-14 20:03:52 +0300

    1

--- src/test/java/Start.java
+++ src/test/java/Start.java
@@ -4,9 +4,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-import javax.swing.*;
+
 import java.util.List;
-import java.util.Set;
+
 
 public class Start {
 WebDriver wd;
@@ -71,7 +71,7 @@ wd.close();
     wd=new ChromeDriver();
     wd.navigate().to("file:///C:/Users/User/Downloads/index%20(1).html");
 
-     WebElement item1 = wd.findElement(By.cssSelector("a[href='#item1']"));
+    WebElement item1 = wd.findElement(By.cssSelector("a[href='#item1']"));
     WebElement item2 = wd.findElement(By.cssSelector("a[href='#item2']"));
     WebElement item3 = wd.findElement(By.cssSelector("a[href='#item3']"));
     WebElement item4 = wd.findElement(By.cssSelector("a[href='#item4']"));
@@ -85,4 +85,50 @@ wd.close();
       wd.quit();
 
 }
+    @Test
+    public void fromCssToxPath(){
+
+        wd.findElement(By.cssSelector("a[href]"));
+
+        wd.findElement(By.xpath("//a[@href]"));
+
+        wd.findElement(By.cssSelector("[href ^='/ho']"));;
+
+        wd.findElement(By.cssSelector("div.focus"));
+
+        wd.findElement(By.cssSelector("input[type='text']"));
+
+        wd.findElement(By.cssSelector("div #host"));
+
+        wd.findElement(By.cssSelector("table#country tr:nt-child(3)) td:last-child"));
+
+        wd.findElement(By.cssSelector("div#idName li:first-child"));
+
+        wd.findElement(By.cssSelector("div.sort>div:nth-child(2)"));
+
+
+    }
+
+
+    @Test
+    public void fromXPathToCss(){
+        wd.findElement(By.xpath("//*[@id='host"));
+
+        wd.findElement(By.cssSelector("#host"));
+
+        wd.findElement(By.xpath("//button[@class='submit']"));
+
+        wd.findElement(By.xpath("//div/div/img[@src='paris.jpg']"));
+
+        wd.findElement(By.xpath("//div[@class='first' and @class='list']"));
+
+        wd.findElement(By.xpath("//div[contains(@class,'st']"));
+
+        wd.findElement(By.xpath("//div[@id=’idName’]//h1[last()]"));
+
+        wd.findElement(By.xpath("//table[@id]//tr[last()]"));
+
+        wd.findElement(By.xpath("//a[starts-with(@id,'ret')]"));
+
+    }
 }
